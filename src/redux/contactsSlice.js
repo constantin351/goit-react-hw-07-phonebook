c5a66a2c247c069537b0f037c0b752563f4135d0
Konstantin Martynenko <konstantin.vents@gmail.com>
2023-06-09 12:50:35 +0300

    updated ContactSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -45,8 +45,9 @@ export const contactsSlice = createSlice({
         [deleteContact.fulfilled](state, action) {
             state.isLoading = false;
             state.error = null;
+
             const index = state.contacts.findIndex(
-                contact => contact.id === action.payload);
+                contact => contact.id === action.payload.id);
             state.contacts.splice(index, 1);
         },
         [deleteContact.rejected]: handleRejected,
